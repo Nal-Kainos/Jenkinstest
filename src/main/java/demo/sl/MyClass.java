961b5b5049ed9896e20bb1881fdc6aee2013f4aa
Nalini Jayaram <nalini.jayaram@kainos.com>
2023-11-21 19:33:48 +0000

    Adding All the files

---
+++ src/main/java/demo/sl/MyClass.java
@@ -0,0 +1,27 @@
+package demo.sl;
+
+public class MyClass {
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	public int sub(int a, int b)
+	{
+		return a-b;
+	}
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
+	public int div(int a, int b)
+	{
+		return a/b;
+	}
+	public static void main(String[] args) {		
+		MyClass ob = new MyClass();
+		System.out.println("Sum is : "+ob.sum(20, 10));
+		System.out.println("Difference is : "+ob.sub(20, 10));
+		System.out.println("Multiply is : "+ob.mul(20, 10));
+		System.out.println("Division is : "+ob.div(20, 10));
+	}
+}
